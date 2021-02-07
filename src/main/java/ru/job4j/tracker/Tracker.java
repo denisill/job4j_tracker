46d310fac9a2fefa399ee388df90256518eb3f58
Denis_illarionov <denisill90@yandex.ru>
2021-02-08 00:58:34 +0300

    6. Метод замены заявки. Tracker.replace [#447268]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,16 +13,22 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -39,5 +45,16 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(items, count);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = false;
+        if (index != -1) {
+            items[index] = item;
+            item.setId(id);
+            result = true;
+        }
+        return result;
     }
 }
