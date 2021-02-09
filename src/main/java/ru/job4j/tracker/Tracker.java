1a5f4e5602e76376f7f18f58cd575c7266651581
Denis_illarionov <denisill90@yandex.ru>
2021-02-10 00:19:17 +0300

    2.1. Реализация класса StartUI [#447205] внес правку

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -40,7 +40,7 @@ public class Tracker {
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (key.equals(item.getName())) {
-                items[index] = item;
+                items[count] = item;
                 count++;
             }
         }
