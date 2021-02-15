0a0a15bbaf89c6349bf2fded62d893ea0471819c
Denis_illarionov <denisill90@yandex.ru>
2021-02-15 09:25:11 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#447217]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,18 +2,13 @@ package ru.job4j.ex;
 
 public class Fact {
     public static void main(String[] args) {
-        Fact fact = new Fact();
-        System.out.println(fact.calc(-1));
+        int rsl = calc(3);
+        System.out.println(rsl);
     }
 
-    public int calc(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Число должно быть больше или равно нулю");
-        }
-        int rsl = 1;
-        for (int index = 1; index <= n; index++) {
-            rsl *= index;
-        }
-        return rsl;
+    public static int calc(int n) {
+        if (n == 0 || n == 1){
+            return 1;
+        } else return calc(n - 1) * n;
     }
 }
