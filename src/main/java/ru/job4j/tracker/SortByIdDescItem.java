73269b605a9d7e6eec87a91ed648110e6a39595d
Denis_illarionov <denisill90@yandex.ru>
2021-03-02 14:25:51 +0300

    0. Сортировка [#447170] fix

--- src/main/java/ru/job4j/tracker/SortByIdDescItem.java
+++ src/main/java/ru/job4j/tracker/SortByIdDescItem.java
@@ -6,10 +6,6 @@ public class SortByIdDescItem implements Comparator<Item> {
 
     @Override
     public int compare(Item o1, Item o2) {
-        if (o1.getId() - o2.getId() > 0) {
-            return -1;
-        } else if (o1.getId() - o2.getId() < 0) {
-            return 1;
-        } else return 0;
+        return o2.compareTo(o1);
     }
 }
