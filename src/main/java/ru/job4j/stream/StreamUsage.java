245052e4cdeb752a4ce54cc062a3b3a4e8784151
Denis_illarionov <denisill90@yandex.ru>
2021-03-21 23:19:16 +0300

    0.1. Отладка лямбда в IDEA [#447011]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class StreamUsage {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, -5, 4, -4, -4, 0);
-        List<Integer> positiveNumbers = numbers.stream().filter(number -> number >= 0).collect(Collectors.toList());
+        List<Integer> positiveNumbers = numbers.stream()
+                .filter(number -> number >= 0).collect(Collectors.toList());
     }
 }
