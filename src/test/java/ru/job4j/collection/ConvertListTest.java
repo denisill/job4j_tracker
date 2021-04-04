3b64fcbf5fb8f213d5c2d0666f0ee5392456714c
Denis_illarionov <denisill90@yandex.ru>
2021-04-04 11:09:55 +0300

    1. Метод .of [#447016]

--- src/test/java/ru/job4j/collection/ConvertListTest.java
+++ src/test/java/ru/job4j/collection/ConvertListTest.java
@@ -3,11 +3,10 @@ package ru.job4j.collection;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class ConvertListTest {
     @Test
@@ -16,7 +15,7 @@ public class ConvertListTest {
         in.add(new int[] {1});
         in.add(new int[] {2, 3});
         in.add(new int[] {5, 3, 8});
-        List<Integer> expect = Arrays.asList(1, 2, 3, 5, 3, 8);
+        List<Integer> expect = List.of(1, 2, 3, 5, 3, 8);
         assertThat(ConvertList.convert(in), is(expect));
     }
 }
